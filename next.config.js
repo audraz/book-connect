06df2aaa1d95ed3d56baf2872fc8ab790648eb64
Audra Zelvania <audra.zelvania15@gmail.com>
2024-12-19 20:11:53 +0700

    error fixing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["books.google.com"], 
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
